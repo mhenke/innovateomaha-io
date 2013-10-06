ad12eac9caed1765f0d315d73ae5be224c361b48
Mike Henke <henke.mike@gmail.com>
2013-10-06 14:31:06 -0500

    adding partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Innovateomaha::Application.routes.draw do
+  get "welcome/construction"
+
   get "welcome/index"
 
   # The priority is based upon order of creation:
@@ -50,7 +52,7 @@ Innovateomaha::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'welcome#index'
+  root :to => 'welcome#construction'
 
   # See how all your routes lay out with "rake routes"
 
--- test/functional/welcome_controller_test.rb
+++ test/functional/welcome_controller_test.rb
@@ -1,9 +1,13 @@
 require 'test_helper'
 
 class WelcomeControllerTest < ActionController::TestCase
+  test "should get construction" do
+    get :construction
+    assert_response :success
+  end
+
   test "should get index" do
     get :index
     assert_response :success
   end
-
 end
