0228271ee4b2230e59231990939cd2fd6c5e7e58
Mike Henke <henke.mike@gmail.com>
2013-10-18 16:56:46 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Innovateomaha::Application.config.secret_token = '77ab4eb5b8a7975ba61737342847e43a865bf3c109e6877bd89a9f84b37859365bc3ed670c5fda596dad983ce84e2dc1f7f1976d03d58ba4ba7be9faff7f6eee'
