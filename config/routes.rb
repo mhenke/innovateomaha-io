1f7c7a92b1a3808b54a1ab187827a8a2fa0acfea
Mike Henke <henke.mike@gmail.com>
2013-10-19 11:21:38 -0500

    change percent done to 35 and default route to construction

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Innovateomaha::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'static_pages#construction'
 
   # See how all your routes lay out with "rake routes"
 
