7eec1f8c9dcb9356ef44b7b955c5dd3d2e5b61a8
Mike Henke <henke.mike@gmail.com>
2013-10-20 16:55:25 -0500

    fix heroku asset pipeline

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,4 +1,43 @@
 
+
+
+// Slider Revolution
+
+jQuery('.mainslider').revolution(
+{
+		delay:9000,
+		startheight:490,
+		startwidth:950,
+
+		thumbWidth:100,
+		thumbHeight:50,
+		thumbAmount:4,
+
+		onHoverStop:"on",
+		hideThumbs:200,
+		navigationType:"thumb",
+		navigationStyle:"round",
+		navigationArrows:"verticalcentered",
+
+		touchenabled:"on",
+
+		navOffsetHorizontal:0,
+		navOffsetVertical:0,
+		shadow:1,
+		fullWidth:"off"
+});
+
+
+$(function() {
+            $(".contentHover").hover(
+                function() {
+                    $(this).children(".content").fadeTo(200, 0.25).end().children(".hover-content").fadeTo(200, 1).show();
+                },
+                function() {
+                    $(this).children(".content").fadeTo(200, 1).end().children(".hover-content").fadeTo(200, 0).hide();
+                });
+        });
+		
 // Flexi Slider
 //  Target sliders individually with different properties
   $(window).load(function() {
