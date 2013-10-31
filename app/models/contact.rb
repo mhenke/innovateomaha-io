3ee2e7105880a847bbe9f02a6a9ea5fd995649cd
Mike Henke <henke.mike@gmail.com>
2013-10-31 17:45:39 -0500

    error on contact page

---
+++ app/models/contact.rb
@@ -0,0 +1,36 @@
+# app/models/inquiry.rb
+ 
+class Contact
+  extend ActiveModel::Naming
+  include ActiveModel::Conversion
+  include ActiveModel::Validations
+  
+  attr_accessor :name, :email, :subject , :message
+  
+  validates :name, 
+            :presence => true
+
+  validates :subject, 
+            :presence => true
+  
+  validates :email,
+            :format => { :with => /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
+  
+  validates :message,
+            :length => { :minimum => 10, :maximum => 1000 }
+  
+  def initialize(attributes = {})
+    attributes.each do |name, value|
+      send("#{name}=", value)
+    end
+  end
+  
+  def deliver
+    return false unless valid?
+    true
+  end
+      
+  def persisted?
+    false
+  end
+end
