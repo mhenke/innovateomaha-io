9db8c835f549e9e2e139d2d4096da29cfd8f3a2c
Mike Henke <henke.mike@gmail.com>
2013-11-03 15:30:35 -0600

    move tweets to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20131103212337) do
+
+  create_table "testimonies", :force => true do |t|
+    t.string   "tweet_id"
+    t.string   "screen_name"
+    t.text     "content"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
 end
