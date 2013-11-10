606747895bfc054c61cc8045bd354b27b4ca0ae3
Mike Henke <henke.mike@gmail.com>
2013-11-10 11:50:10 -0600

    fixing initializer

--- config/initializers/monologue.rb
+++ config/initializers/monologue.rb
@@ -1,37 +1,34 @@
-Monologue.config do |c|
-  c.site_name = "@jipiboily / Jean-Philippe Boily"
-  c.site_subtitle = "my own place online"
-  c.site_url = "http://jipiboily.com"
+Monologue.site_name = "@jipiboily / Jean-Philippe Boily"
+Monologue.site_subtitle = "my own place online"
+Monologue.site_url = "http://jipiboily.com"
 
-  c.meta_description = "This is my personal blog about Rails, Monologue, programming, etc..."
-  c.meta_keyword = "rails, programming, monologue, ruby"
+Monologue.meta_description = "This is my personal blog about Rails, Monologue, programming, etc..."
+Monologue.meta_keyword = "rails, programming, monologue, ruby"
 
-  c.admin_force_ssl = false
-  c.posts_per_page = 10
+Monologue.admin_force_ssl = false
+Monologue.posts_per_page = 10
 
-  c.disqus_shortname = "jipiboily"
+Monologue.disqus_shortname = "jipiboily"
 
-  # LOCALE
-  c.twitter_locale = "en" # "fr"
-  c.facebook_like_locale = "en_US" # "fr_CA"
-  c.google_plusone_locale = "en"
+# LOCALE
+Monologue.twitter_locale = "en" # "fr"
+Monologue.facebook_like_locale = "en_US" # "fr_CA"
+Monologue.google_plusone_locale = "en"
 
-  # c.layout               = "layouts/application"
+# c.layout               = "layouts/application"
 
-  # ANALYTICS
-  # c.gauge_analytics_site_id = "YOUR COGE FROM GAUG.ES"
-  # c.google_analytics_id = "YOUR GA CODE"
+# ANALYTICS
+# c.gauge_analytics_site_id = "YOUR COGE FROM GAUG.ES"
+# c.google_analytics_id = "YOUR GA CODE"
 
-  c.sidebar = ["latest_posts", "latest_tweets", "categories", "tag_cloud"]
+Monologue.sidebar = ["latest_posts", "latest_tweets", "categories", "tag_cloud"]
 
 
-  #SOCIAL
-  c.twitter_username = "jipiboily"
-  c.facebook_url = "https://www.facebook.com/jipiboily"
-  c.facebook_logo = 'logo.png'
-  c.google_plus_account_url = "https://plus.google.com/u/1/115273180419164295760/posts"
-  c.linkedin_url = "http://www.linkedin.com/in/jipiboily"
-  c.github_username = "jipiboily"
-  c.show_rss_icon = true
-
-end
+#SOCIAL
+Monologue.twitter_username = "jipiboily"
+Monologue.facebook_url = "https://www.facebook.com/jipiboily"
+Monologue.facebook_logo = 'logo.png'
+Monologue.google_plus_account_url = "https://plus.google.com/u/1/115273180419164295760/posts"
+Monologue.linkedin_url = "http://www.linkedin.com/in/jipiboily"
+Monologue.github_username = "jipiboily"
+Monologue.show_rss_icon = true
