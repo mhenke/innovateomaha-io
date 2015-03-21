eae5d942e738bf03b56865b38ece8c724cee88f3
Mike Henke <henke.mike@gmail.com>
2015-03-20 23:36:54 -0500

    first commit

--- server.js
+++ server.js
@@ -1,36 +1,56 @@
 'use strict';
-/**
- * Module dependencies.
- */
-var init = require('./config/init')(),
-	config = require('./config/config'),
-	mongoose = require('mongoose'),
-	chalk = require('chalk');
 
-/**
- * Main application entry file.
- * Please note that the order of loading is important.
- */
+/*
+var cl = console.log;
+console.log = function(){
+  console.trace();
+  cl.apply(console,arguments);
+};
+*/
 
-// Bootstrap db connection
-var db = mongoose.connect(config.db, function(err) {
-	if (err) {
-		console.error(chalk.red('Could not connect to MongoDB!'));
-		console.log(chalk.red(err));
-	}
-});
+// Requires meanio .
+var mean = require('meanio');
+var cluster = require('cluster');
+var mongoose = require('mongoose');
 
-// Init the express application
-var app = require('./config/express')(db);
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://mean:mean@ds031711.mongolab.com:31711/heroku_app34893074');
+app.use(express.static(__dirname + '/build'));
 
-// Bootstrap passport config
-require('./config/passport')();
+// Code to run if we're in the master process or if we are not in debug mode/ running tests
 
-// Start the app by listening on <port>
-app.listen(config.port);
+if ((cluster.isMaster) && (process.execArgv.indexOf('--debug') < 0) && (process.env.NODE_ENV!=='test') && (process.execArgv.indexOf('--singleProcess')<0)) {
+//if (cluster.isMaster) {
 
-// Expose app
-exports = module.exports = app;
+    console.log('for real!');
+    // Count the machine's CPUs
+    var cpuCount = require('os').cpus().length;
 
-// Logging initialization
-console.log('MEAN.JS application started on port ' + config.port);
+    // Create a worker for each CPU
+    for (var i = 0; i < cpuCount; i += 1) {
+        console.log ('forking ',i);
+        cluster.fork();
+    }
+
+    // Listen for dying workers
+    cluster.on('exit', function (worker) {
+        // Replace the dead worker, we're not sentimental
+        console.log('Worker ' + worker.id + ' died :(');
+        cluster.fork();
+
+    });
+
+// Code to run if we're in a worker process
+} else {
+
+    var workerId = 0;
+    if (!cluster.isMaster)
+    {
+        workerId = cluster.worker.id;
+    }
+// Creates and serves mean application
+    mean.serve({ workerid: workerId /* more options placeholder*/ }, function (app) {
+      var config = app.config.clean;
+        var port = config.https && config.https.port ? config.https.port : config.http.port;
+        console.log('Mean app started on port ' + port + ' (' + process.env.NODE_ENV + ') cluster.worker.id:', workerId);
+    });
+}
